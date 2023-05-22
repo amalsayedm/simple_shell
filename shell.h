525f143599151cd41fa20c113f832312387a666d
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-05-22 09:20:28 +0300

    h

--- shell.h
+++ shell.h
@@ -106,7 +106,7 @@ typedef struct data_passed
 	int count_history;
 } inputs_t;
 
-#define data_INIT \
+#define INFO_INIT \
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
 	0, 0, 0}
 
