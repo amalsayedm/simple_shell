13add92792805d7e37a6792e61e75b7fc1dbd245
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-05-22 06:42:26 +0300

    Update alias.c

--- alias.c
+++ alias.c
@@ -1,5 +1,59 @@
 #include "shell.h"
 
+
+/**
+ * alias - built in alias
+ * @data: pointer
+ * Return: 0
+ */
+
+int alias(inputs_t *data)
+{
+	int index = 0;
+	char *ptr = NULL;
+	link_t *node = NULL;
+
+	if (data->ac == 1)
+	{
+		node = data->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (index = 1; data->av[index]; index++)
+	{
+		ptr = _strchr(data->av[index], '=');
+		if (ptr)
+			set_alias(data, data->av[index]);
+		else
+			print_alias(begain_with_node(data->alias, data->av[index], '='));
+	}
+	return (0);
+}
+/**
+ * set_alias - sets alias
+  * @data: pointer
+ * @str:  str alias
+ * Return: 0 or 1
+ */
+
+int set_alias(inputs_t *data, char *str)
+{
+	char *ptr;
+
+	ptr = _strchr(str, '=');
+	if (!ptr)
+		return (1);
+	if (!*++ptr)
+		return (unset_alias(data, str));
+	unset_alias(data, str);
+	return (addnode_to_end(&(data->alias), str, 0) == NULL);
+}
+
+
 /**
  * unset_alias - sets alias to str
  * @data: parameter struct
@@ -46,59 +100,4 @@ int print_alias(link_t *node)
 		return (0);
 	}
 	return (1);
-}
-
-
-/**
- * set_alias - sets alias
-  * @data: pointer
- * @str:  str alias
- * Return: 0 or 1
- */
-
-int set_alias(inputs_t *data, char *str)
-{
-	char *ptr;
-
-	ptr = _strchr(str, '=');
-	if (!ptr)
-		return (1);
-	if (!*++ptr)
-		return (unset_alias(data, str));
-	unset_alias(data, str);
-	return (addnode_to_end(&(data->alias), str, 0) == NULL);
-}
-
-
-/**
- * alias - built in alias
- * @data: pointer
- * Return: 0
- */
-
-int alias(inputs_t *data)
-{
-	int index = 0;
-	char *ptr = NULL;
-	link_t *node = NULL;
-
-	if (data->ac == 1)
-	{
-		node = data->alias;
-		while (node)
-		{
-			print_alias(node);
-			node = node->next;
-		}
-		return (0);
-	}
-	for (index = 1; data->av[index]; index++)
-	{
-		ptr = _strchr(data->av[index], '=');
-		if (ptr)
-			set_alias(data, data->av[index]);
-		else
-			print_alias(begain_with_node(data->alias, data->av[index], '='));
-	}
-	return (0);
 }
